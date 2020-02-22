dc9ac3ff00619f1e3f7ee736545536d95688d276
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 14:05:23 -0500

    added onClick listener

--- Hello-Toast/app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ Hello-Toast/app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.hellotoast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mCount = 0
+
+        button_count.setOnClickListener {
+            mCount++
+            show_count.text = mCount.toString()
+        }
+
+        button_toast.setOnClickListener {
+            Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
