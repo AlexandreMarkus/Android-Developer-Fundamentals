4ea419c424b70f24a91b9e93e3b95e724e2e5bb2
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 17:51:59 -0500

    returned data back to main activity

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.twoactivities
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,9 +20,26 @@ class MainActivity : AppCompatActivity() {
         button_main.setOnClickListener {
             Log.d(tag, "Button clicked!")
             val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            intent.putExtra("EXTRA_MESSAGE", "${editText_main.text}")
+            startActivityForResult(intent, 1)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == Activity.RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                text_header_reply.visibility = View.VISIBLE
+                text_message_reply.visibility = View.VISIBLE
+                text_message_reply.text = data?.getStringExtra("EXTRA_REPLY")
+                // Do something with the contact here (bigger example below)
+            }
         }
     }
-
 
 }
--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -1,12 +1,27 @@
 package com.example.twoactivities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        val message = intent.getStringExtra("EXTRA_MESSAGE")
+        text_message.text = message
+
+        button_second.setOnClickListener {
+            val replyIntent = Intent()
+            replyIntent.putExtra("EXTRA_REPLY", "${editText_second.text}")
+            setResult(Activity.RESULT_OK, replyIntent)
+            finish()
+        }
     }
 }
