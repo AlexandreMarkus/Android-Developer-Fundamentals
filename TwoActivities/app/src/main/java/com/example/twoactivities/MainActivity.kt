0c7bbccd759cd5338a217347249838309a3221db
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 16:19:38 -0500

    added intent for second activity

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.twoactivities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val LOG_TAG = MainActivity::class.java.simpleName
+
+        button_main.setOnClickListener {
+            Log.d(LOG_TAG, "Button clicked!")
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
