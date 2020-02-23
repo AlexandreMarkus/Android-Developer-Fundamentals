a581b2e444d1347990a1056e2b35b413049a4da5
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 22:37:52 -0500

    typo removed warning

--- ImplicitIntentsReceiver/app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
+++ ImplicitIntentsReceiver/app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.implicitintentsreceiver
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
