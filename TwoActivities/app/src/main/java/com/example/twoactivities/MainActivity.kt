acdee578f8a8fede1cd42d514a36254ec7fe12f3
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 21:24:26 -0500

    lesson 02.2: Activity lifecycle and state

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -17,6 +17,24 @@ class MainActivity : AppCompatActivity() {
 
         val tag = MainActivity::class.java.simpleName
 
+        Log.d(tag, "-------");
+        Log.d(tag, "onCreate");
+
+        // Restore the saved state.
+        // See onSaveInstanceState() for what gets saved.
+        // Restore the saved state.
+// See onSaveInstanceState() for what gets saved.
+        if (savedInstanceState != null) {
+            val isVisible = savedInstanceState.getBoolean("reply_visible")
+            // Show both the header and the message views. If isVisible is
+// false or missing from the bundle, use the default layout.
+            if (isVisible) {
+                text_header_reply.visibility = View.VISIBLE
+                text_message_reply.text = savedInstanceState.getString("reply_text")
+                text_message_reply.visibility = View.VISIBLE
+            }
+        }
+
         button_main.setOnClickListener {
             Log.d(tag, "Button clicked!")
             val intent = Intent(this, SecondActivity::class.java)
@@ -25,6 +43,17 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // If the heading is visible, message needs to be saved.
+// Otherwise we're still using default layout.
+        if (text_header_reply.visibility == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true)
+            outState.putString("reply_text", "${text_message_reply.text}")
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         // Check which request we're responding to
