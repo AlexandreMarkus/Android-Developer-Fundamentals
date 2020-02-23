525366f2594e12f215aed0e9cff6982644b361ac
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-22 22:34:16 -0500

    Receive an implicit Intent

---
+++ ImplicitIntentsReceiver/app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.implicitintentsreceiver
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = intent
+        val uri: Uri? = intent.data
+        if (uri != null) {
+            text_uri_message.text = uri.toString()
+        }
+    }
+}
