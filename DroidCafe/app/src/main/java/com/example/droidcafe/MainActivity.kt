590d75c4be2326fd953adfc829e7e295425f3f5b
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-02-28 17:01:17 -0500

    added onclick for images

--- DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.kt
+++ DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.droidcafe
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,4 +39,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+    
 }
