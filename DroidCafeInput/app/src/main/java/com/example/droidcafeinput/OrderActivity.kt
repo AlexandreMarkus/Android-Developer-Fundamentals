7f161fc50d2637d94e77a0a846861246f5c5e54e
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-03-16 15:06:03 -0400

    lesson 4.02 - Task 2: Use radio buttons

--- DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.kt
+++ DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.kt
@@ -1,6 +1,7 @@
 package com.example.droidcafeinput
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.droidcafe.R
 import kotlinx.android.synthetic.main.activity_order.*
@@ -17,5 +18,29 @@ class OrderActivity : AppCompatActivity() {
         // Get the intent and its data.
         val message = intent.getStringExtra("EXTRA_MESSAGE")
         order_textview.text = message
+
+        sameday_radioButton.setOnClickListener{
+            displayToast(getString(R.string.same_day_radiotext))
+        }
+
+        nextday_radioButton.setOnClickListener{
+            displayToast(getString(R.string.next_day_radiotext))
+        }
+
+        pickup_radioButton.setOnClickListener {
+            displayToast(getString(R.string.pick_up_radiotext))
+        }
+    }
+
+    /**
+     * Displays a Toast with the message.
+     *
+     * @param message Message to display
+     */
+    private fun displayToast(message: String?) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
