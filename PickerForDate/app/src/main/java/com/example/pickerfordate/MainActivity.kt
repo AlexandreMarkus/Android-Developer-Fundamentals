480fb8371dba47b6717b7f9ab17be8ad8fec628c
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-03-16 19:22:38 -0400

    lesson 04.3 - Task 5: Use a picker for user input

---
+++ PickerForDate/app/src/main/java/com/example/pickerfordate/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.pickerfordate
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            Toast.makeText(this, "Date: " + sdf.format(cal.time),
+                    Toast.LENGTH_SHORT).show()
+        }
+
+        button.setOnClickListener {
+            DatePickerDialog(this@MainActivity, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+}
