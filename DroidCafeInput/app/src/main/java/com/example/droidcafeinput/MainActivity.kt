492f110020737640bf9b8817308e6410f9ee26cd
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-03-16 18:12:53 -0400

    lesson 4.03 - Task 2: Add icons for menu items

--- DroidCafeInput/app/src/main/java/com/example/droidcafeinput/MainActivity.kt
+++ DroidCafeInput/app/src/main/java/com/example/droidcafeinput/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity() {
 
         // This comment suppresses the Android Studio warning about simplifying
         // the return statements.
-        return if (id == R.id.action_settings) {
+        return if (id == R.id.order_settings) {
             true
         } else super.onOptionsItemSelected(item)
     }
