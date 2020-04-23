04668adcbf19cbaf2ac7e3c3a72984243d2c150f
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-04-23 14:48:01 -0400

    lesson 05.1 - Task 4: Themes and final touches

--- Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.scorekeeper
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val STATE_SCORE_1 = "Team 1 Score"
+const val STATE_SCORE_2 = "Team 2 Score"
 
 class MainActivity : AppCompatActivity() {
     private var mScore1: Int = 0
@@ -12,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            score_1.text = mScore1.toString()
+            score_2.text = mScore2.toString()
+        }
     }
 
     /**
@@ -52,5 +65,42 @@ class MainActivity : AppCompatActivity() {
                 score_2.text = mScore2.toString()
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Check if the correct item was clicked
+        if(item.itemId==R.id.night_mode) {
+            // Get the night mode state of the app.
+            val nightMode: Int = AppCompatDelegate.getDefaultNightMode()
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+        }
+// Recreate the activity for the theme change to take effect.
+            recreate();
+            return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu!!.findItem(R.id.night_mode).setTitle(R.string.day_mode)
+        } else {
+            menu!!.findItem(R.id.night_mode).setTitle(R.string.night_mode)
+        }
+        return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScore1)
+        outState.putInt(STATE_SCORE_2, mScore2)
+        super.onSaveInstanceState(outState)
     }
 }
